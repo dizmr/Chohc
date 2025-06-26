93c946229e6898a064f717cba24efd2b8ba8dac1
dizmr <sergeenko.saveli@gmail.com>
2025-06-26 19:57:14 +0300

    Initial commit from new Rider project

--- Server/Services/TarkovPriceChecker.cs
+++ Server/Services/TarkovPriceChecker.cs
@@ -1,111 +1,83 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Dynamic;
+using System.Linq;
 using System.Net;
-using System.Net.Http.Json;
 using System.Net.Sockets;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
-
-class User
-{
-    public string Username { get; set; }
-    public IPEndPoint IPAddress { get; set; }
-    public List<Item> TracItems { get; set; } = new List<Item>(); 
-
-    public User(string username, IPEndPoint iPAddress)
-    {
-        Username = username;
-        IPAddress = iPAddress;
-    }
-
-    public void ShowUserInfo()
-    {
-        Console.WriteLine($"Nickname: {Username} | IP: {IPAddress}");
-
-        if (TracItems.Count > 0)
-        {
-            Console.WriteLine("Traced items:");
-            foreach (var item in TracItems)
-            {
-                Console.WriteLine("  - " + item); 
-            }
-        }
-        else
-        {
-            Console.WriteLine("Error.");
-        }
-    }
-
-    public void AddTrackedItem(Item item)
-    {
-
-        TracItems.Add(item);
-        Console.WriteLine($"Item added: {item.name}");
-    }
-}
-
 
 public class Item
 {
-    public string id { get; set; }
-    public string name { get; set; }
-    public int? avg24hPrice { get; set; }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public int? Avg24hPrice { get; set; }
 
     public override string ToString()
     {
-        return $"ID: {id} | Name: {name} | Price: {(avg24hPrice.HasValue ? avg24hPrice.Value.ToString() + "P" :"No Data")}";
+        return $"ID: {Id} | Name: {Name} | Price: {(Avg24hPrice.HasValue ? Avg24hPrice.Value.ToString() + "₽" : "No Data")}";
     }
 }
 
 public class ApiResponse
 {
-    public ItemData data { get; set; }
+    public ItemData Data { get; set; }
 }
 
 public class ItemData
 {
-    public List<Item> items { get; set; }
+    public List<Item> Items { get; set; }
+}
+
+public class User
+{
+    public string Username { get; set; }
+    public IPEndPoint IPAddress { get; set; }
+    public List<Item> TrackedItems { get; set; } = new List<Item>();
+
+    public User(string username, IPEndPoint ip)
+    {
+        Username = username;
+        IPAddress = ip;
+    }
+
+    public void AddTrackedItem(Item item)
+    {
+        lock (TrackedItems)
+        {
+            TrackedItems.Add(item);
+        }
+        Console.WriteLine($"[{Username}] Item added: {item.Name}");
+    }
+
+    public void ShowUserInfo()
+    {
+        Console.WriteLine($">>> Nickname: {Username} | IP: {IPAddress}");
+        lock (TrackedItems)
+        {
+            if (TrackedItems.Count == 0)
+            {
+                Console.WriteLine("  No items tracked.");
+                return;
+            }
+            Console.WriteLine("  Tracked items:");
+            foreach (var item in TrackedItems)
+            {
+                Console.WriteLine("   - " + item);
+            }
+        }
+    }
 }
 
 class Program
 {
     static int port = 5582;
-    static List<User> clients = new List<User>();
+    static ConcurrentDictionary<string, User> clients = new ConcurrentDictionary<string, User>();
 
-    public static async Task GetAll()
-    {
-        var data = new Dictionary<string, string>()
-        {
-            {"query", "{items {id name shortName}}" }
-        };
-        using (var httpClient = new HttpClient())
-        {
-            var httpResponse = await httpClient.PostAsJsonAsync("https://api.tarkov.dev/graphql", data);
-            var responseContent = await httpResponse.Content.ReadAsStringAsync();
-            Console.WriteLine(responseContent);
-        }
-    }
-
-    public static async Task<string> GetItemInfo(string itemName)
-    {
-        string query = $"{{\"query\":\"{{ items(name: \\\"{itemName}\\\") {{ id name avg24hPrice }} }}\"}}";
-
-        using var httpClient = new HttpClient();
-        var content = new StringContent(query, Encoding.UTF8, "application/json");
-
-        var response = await httpClient.PostAsync("https://api.tarkov.dev/graphql", content);
-        var json = await response.Content.ReadAsStringAsync();
-
-        var result = JsonSerializer.Deserialize<ApiResponse>(json);
-
-        if (result?.data?.items == null  result.data.items.Count == 0)
-            return "Item not found";
-
-        return string.Join("\n", result.data.items.Select(i => i.ToString()));
-    }
-    /*public static async Task AddFirstMatchingItemToUser(User user ,string searchName)
+    public static async Task<Item> GetFirstItem(string searchName)
     {
         string query = $"{{\"query\":\"{{ items(name: \\\"{searchName}\\\") {{ id name avg24hPrice }} }}\"}}";
 
@@ -120,31 +92,47 @@ class Program
             PropertyNameCaseInsensitive = true
         });
 
-        if (result?.data?.items == null  result.data.items.Count == 0)
-        {
-            Console.WriteLine("Ничего не найдено.");
-            return;
-        }
-
-        var firstItem = result.data.items.FirstOrDefault();
-        if (firstItem != null)
-        {
-            user.AddTrackedItem(firstItem);
-        }
-    }*/
-
+        return result?.Data?.Items?.FirstOrDefault();
+    }
 
     static async Task Main(string[] args)
     {
-        Console.WriteLine("Server started...");
+        Console.WriteLine("Server started on port " + port);
         UdpClient server = new UdpClient(port);
-        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, port);
-        await GetAll();
-        var info = await GetItemInfo("AR-15 Colt charging handle");
-        Console.WriteLine(info);
-        /*await GetAll();
-        Console.WriteLine("\n\n\n");
-        string info = await GetItemInfo("");
-        Console.WriteLine(info);*/
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        _ = Task.Run(async () =>
+        {
+            while (!cts.Token.IsCancellationRequested)
+            {
+                var result = await server.ReceiveAsync();
+                var message = Encoding.UTF8.GetString(result.Buffer);
+                var ip = result.RemoteEndPoint;
+
+                Task.Run(async () =>
+                {
+                    string clientId = ip.ToString();
+                    var user = clients.GetOrAdd(clientId, _ => new User("User@" + clientId, ip));
+
+                    if (message.StartsWith("track:"))
+                    {
+                        string itemName = message.Substring(6).Trim();
+                        var item = await GetFirstItem(itemName);
+                        if (item != null)
+                            user.AddTrackedItem(item);
+                        else
+                            Console.WriteLine($"Item not found: {itemName}");
+                    }
+                    else if (message == "info")
+                    {
+                        user.ShowUserInfo();
+                    }
+                });
+            }
+        });
+
+        Console.WriteLine("Press Enter to stop the server...");
+        Console.ReadLine();
+        cts.Cancel();
     }
 }
